ea544a40db9173dfdc35abb8782976b44e4a4ebd
Krishna2217 <rautkrishna2217@gmail.com>
2023-06-18 03:06:10 +0545

    contact page completed

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>
+                    TechStar
+                </h1>
+                <p>@all right reserved</p>
+            </div>
+
+            <div>
+                <h5>
+                    Follow Us
+                </h5>
+                <div>
+                    <a href="https://www.instagram.com/_krishna_v_2.0/" target={'blank'}>Youtube</a>
+                    <a href="https://www.instagram.com/_krishna_v_2.0/" target={'blank'}>Instagram</a>
+                    <a href="https://www.instagram.com/_krishna_v_2.0/" target={'blank'}> Github</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import vg from "../assets/2.webp";
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai";
 const Home = () => {
     return (
     <>
-        <div className='home'>
+        <div className='home' id='home'>
             <main>
             <h1>TechStar</h1>
             <p>Solution to all you problems</p>
@@ -18,13 +19,39 @@ const Home = () => {
                 </p>
             </div>
         </div>
-        <div className="home3">
+        <div className="home3" id="about">
             <div>
 
                 <h1>Who we are</h1>
                 <p>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quae perferendis pariatur voluptates numquam atque placeat quasi cum quis quaerat cupiditate maiores consequuntur! Dolorem totam nesciunt eaque perspiciatis rerum ab sequi consequuntur asperiores cumque, quisquam quis? Quasi modi sunt nihil aut quaerat. Expedita provident eligendi reiciendis et accusantium pariatur reprehenderit obcaecati voluptates, eveniet rerum laboriosam, nostrum beatae, qui molestias ducimus vel. Qui enim itaque voluptates id quam earum nobis quae, vitae aliquid odio tenetur minima quos. Ex corrupti tempora cum, neque amet asperiores, itaque illum, facilis temporibus beatae animi repudiandae atque nam vitae dolore. Ratione ex similique nemo rem quos?
                 </p>
+            </div>
+        </div>
+        <div className="home4" id="brands">
+            <div>
+                <h1>Brands</h1>
+                <article>
+                    <div style={{animationDelay:"0.3s"}}>
+                    <AiFillGoogleCircle />
+                    <p>Google</p>
+                    </div>
+
+                    <div style={{animationDelay:"0.5s"}}>
+                    <AiFillAmazonCircle />
+                    <p>Amazon</p>
+                    </div>
+
+                    <div style={{animationDelay:"0.7s"}}>
+                    <AiFillYoutube />
+                    <p>Youtube</p>
+                    </div>
+
+                    <div style={{animationDelay:"0.1s"}}>
+                    <AiFillInstagram />
+                    <p>Instagram</p>
+                    </div>
+                </article>
             </div>
         </div>
     </>
