01a4de5eeb681e10b947d5510bbec166a74d2290
Krishna2217 <rautkrishna2217@gmail.com>
2023-06-18 17:17:27 +0545

    medai query applied

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import Services from './components/Services'
 
 import "./styles/App.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/Contact.scss"
+import "./styles/mediaquery.scss"
+
 
 
 const App = () => {
@@ -19,6 +22,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer/>
     </Router>
